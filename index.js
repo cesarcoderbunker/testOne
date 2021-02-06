74ae4b9f7596079683e3b7f2e9efb9a587d7bfa1
cesar <cesar.hertz@icloud.com>
2021-02-06 13:17:34 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,38 @@
+const express =  require('express');
+const {readFile} = require('fs')
+
+
+
+
+const app = express()
+
+app.get('/', (request,response)=>{
+    readFile('./text.txt', 'utf8' , (err, data) => {
+        if (err) {
+          console.error("err", err)
+          return
+        }
+        response.send(data);
+      })
+})
+
+app.get('/viva', (req,res)=>{
+    console.log('bam bam bam')
+    })
+
+    app.get('/test', (request,response)=>{
+
+        readFile('./index.html', 'utf8' , (err, data) => {
+            if (err) {
+              console.error("err", err)
+              return
+            }
+            response.send(data);
+          })
+    
+    
+    })
+
+
+
+app.listen(process.env.PORT || 3000, () => console.log('App available at localhost 3000s'))
